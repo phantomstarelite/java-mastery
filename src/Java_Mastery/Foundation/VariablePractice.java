75fbf0036cfd355955a226d5c5f0ce8c372888b5
phantomstarelite <pratikkhumkar2004@gmail.com>
2026-01-25 02:41:46 +0530

    feat: add variable and variablespractice file in repo

---
+++ src/Java_Mastery/Foundation/VariablePractice.java
@@ -0,0 +1,13 @@
+package Java_Mastery.Foundation;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        String name = "Pratik";
+        int age = 21;
+        boolean isLearningJava = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Learning Java: " + isLearningJava);
+    }
+}
---
+++ src/Java_Mastery/Foundation/Variables.java
@@ -0,0 +1,18 @@
+package Java_Mastery.Foundation;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Pratik";
+        int age = 21;
+        float percentage  = 85.5f;
+        char grade = 'A';
+        boolean isLearningJava = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(percentage);
+        System.out.println(grade);
+        System.out.println(isLearningJava);
+
+    }
+}
